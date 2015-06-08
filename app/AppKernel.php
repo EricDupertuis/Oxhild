df6d9165fe1eeb60d8f54d7317b105651f0c391b
EricDupertuis <dupertuis.eric@gmail.com>
2015-06-08 22:24:11 +0200

    Register FosUserBundle and configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Oxhild\MtgBundle\OxhildMtgBundle(),
             new Oxhild\ImportBundle\OxhildImportBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Oxhild\UserBundle\OxhildUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

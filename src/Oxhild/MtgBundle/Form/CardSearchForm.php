5af5c8eb13e5940a9c329e56bae9a367a670ecf9
EricDupertuis <dupertuis.eric@gmail.com>
2015-06-14 23:00:41 +0200

    Search form class

---
+++ src/Oxhild/MtgBundle/Form/CardSearchForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oxhild\MtgBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CardSearchForm extends AbstractType
+{
+    /**
+     * Builds the SearchCard form
+     * @param  \Symfony\Component\Form\FormBuilder $builder
+     * @param  array $options
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Oxhild\MtgBundle\Entity\Card'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'oxhild_search_card';
+    }
+}

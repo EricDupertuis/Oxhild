7120d9a107c1fe9ac527dcc99c2d3df24d4c30a1
EricDupertuis <dupertuis.eric@gmail.com>
2015-06-22 23:03:07 +0200

    wip

---
+++ src/Oxhild/MtgBundle/Controller/BinderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Oxhild\MtgBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Oxhild\MtgBundle\Form\NewBinder;
+use Oxhild\MtgBundle\Entity\Binder;
+
+class BinderController extends Controller
+{
+
+    public function newAction()
+    {
+        $form = $this->container->get('form.factory')->create(new NewBinder());
+        $binder = new Binder();
+
+        if ($form->isValid()) {
+            
+        }
+
+        return $this->render('OxhildMtgBundle:Binder:new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+}
--- src/Oxhild/MtgBundle/Form/NewBinder.php
+++ src/Oxhild/MtgBundle/Form/NewBinder.php
@@ -9,6 +9,25 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class NewBinder extends AbstractType
 {
 
+    /**
+     * New Binder Form
+     * @param  \Symfony\Component\Form\FormBuilder $builder
+     * @param  array $options
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Oxhild\MtgBundle\Entity\Binder'
+        ));
+    }
 
     public function getName()
     {

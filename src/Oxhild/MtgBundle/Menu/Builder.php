46ff19d662a9b0c745bc8c8c1b2010d8e7e11d1c
EricDupertuis <dupertuis.eric@gmail.com>
2015-07-05 22:36:25 +0200

    knp main menu

--- src/Oxhild/MtgBundle/Menu/Builder.php
+++ src/Oxhild/MtgBundle/Menu/Builder.php
@@ -9,7 +9,14 @@ class Builder extends ContainerAware
 {
     public function mainMenu(FactoryInterface $factory, array $options)
     {
-        $menu = $factory->createItem('root');
+        $menu = $factory->createItem(
+            'root',
+            array(
+                'childrenAttributes' => array(
+                    'class' => 'nav navbar-nav navbar-right'
+                )
+            )
+        );
 
         $menu->addChild('Home', array('route' => 'oxhild_homepage'));
 

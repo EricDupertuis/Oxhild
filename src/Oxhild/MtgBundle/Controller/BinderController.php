be5517eb52b5cafa8084a34df9777fb68a5beacf
EricDupertuis <dupertuis.eric@gmail.com>
2015-07-29 20:04:01 +0200

    Register new binder

--- src/Oxhild/MtgBundle/Controller/BinderController.php
+++ src/Oxhild/MtgBundle/Controller/BinderController.php
@@ -5,19 +5,40 @@ namespace Oxhild\MtgBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Oxhild\MtgBundle\Form\NewBinderForm;
 use Oxhild\MtgBundle\Entity\Binder;
+use Symfony\Component\HttpFoundation\Request;
 
 class BinderController extends Controller
 {
 
-    public function newAction()
+    public function newAction(Request $request)
     {
         $form = $this->container->get('form.factory')->create(new NewBinderForm());
 
+        $form->handleRequest($request);
+
         if ($form->isValid()) {
-            $binder = $form->getData();
+            $data = $form->getData();
             $em = $this->getDoctrine()->getManager();
+
+            $binder = new Binder();
+            $binder->setName($data['name'])
+                ->setDescription($data['description']);
+
+            if ($data['private'] == 1) {
+                $binder->setPrivate(1);
+            } else {
+                $binder->setPrivate(2);
+            }
+
+            $binder->setAddedDate(new \DateTime("now"))
+                ->setUpdatedDate(new \DateTime("now"));
+
+            $user = $this->getUser();
+            $binder->setUser($user);
             $em->persist($binder);
             $em->flush();
+
+            return $this->redirect($this->generateUrl('oxhild_homepage'));
         }
 
         return $this->render('OxhildMtgBundle:Binder:new.html.twig', array(

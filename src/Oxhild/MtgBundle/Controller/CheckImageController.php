9dc61307d981175717598b53bbe05ae948ff5f69
EricDupertuis <dupertuis.eric@gmail.com>
2015-08-25 21:47:07 +0200

    Added modified

--- src/Oxhild/MtgBundle/Controller/CheckImageController.php
+++ src/Oxhild/MtgBundle/Controller/CheckImageController.php
@@ -1,15 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: eric
- * Date: 8/25/15
- * Time: 8:12 PM
- */
 
 namespace Oxhild\MtgBundle\Controller;
 
+use Oxhild\MtgBundle\Entity\Card;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-class CheckImageController
+class CheckImageController extends Controller
 {
-
+    public function checkImageAction($multiverse)
+    {
+        $fs = new Filesystem();
+        if ($fs->exists("/scans/".$multiverse.".jpg")) {
+            return $this->render(
+                "OxhildMtgBundle:Card:scanImage.html.twig",
+                [
+                    'id' => $multiverse
+                ]
+            );
+        } else {
+            return $this->render(
+                "OxhildMtgBundle:Card:scanImage.html.twig",
+                [
+                    'id' => $multiverse
+                ]
+            );
+        }
+    }
 }

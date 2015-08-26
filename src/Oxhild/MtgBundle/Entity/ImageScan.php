09f0a32c9a974e5ac709c6365ece68c2e17dc720
EricDupertuis <dupertuis.eric@gmail.com>
2015-08-26 10:40:05 +0200

    Image to ImageScan

---
+++ src/Oxhild/MtgBundle/Entity/ImageScan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Oxhild\MtgBundle\Entity;
+
+/**
+ * Image entity
+ *
+ * @package Oxhild\MtgBundle\Entity
+ *
+ * @author Eric Dupertuis <dupertuis.eric@gmail.com>
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="colors")
+ */
+class ImageScan
+{
+
+    protected $id;
+
+    protected $name;
+
+    protected $extension;
+
+    protected
+}
